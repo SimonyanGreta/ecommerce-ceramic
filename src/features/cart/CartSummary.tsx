85dfcfda18f17b0efe5b224c7623d4890abbefce
greta <greta.simonyan98@gmail.com>
2025-10-22 14:20:00 +0400

    add closeCart when onpen Checkout page

--- src/features/cart/CartSummary.tsx
+++ src/features/cart/CartSummary.tsx
@@ -1,8 +1,10 @@
 import { useCart } from "../../hooks/useCart";
 import { Link } from "react-router-dom";
+import { useCartDrawer } from "../../hooks/useCartDrawer.ts";
 
 export const CartSummary = () => {
   const { subtotal, count, clear } = useCart();
+  const { closeCart } = useCartDrawer();
 
   return (
     <div className="sticky bottom-0 bg-white/80 backdrop-blur pt-3 border-t border-black/10">
@@ -15,6 +17,7 @@ export const CartSummary = () => {
         <Link
           to="/checkout"
           className="flex-1 inline-flex items-center justify-center rounded-xl px-4 py-2 border border-black/10 hover:bg-black hover:text-white transition"
+          onClick={() => closeCart()}
         >
           Checkout
         </Link>
