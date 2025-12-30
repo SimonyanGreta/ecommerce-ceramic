86ba1254c13a9cfec40ecb4bf3db8f16e339fb8e
greta <greta.simonyan98@gmail.com>
2025-12-30 15:20:00 +0400

    move i18n.js into src/config

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./styles/index.css";
-import "../config/i18n.js";
+import "./config/i18n.js";
 
 import App from "./app/App.tsx";
 import { initCartSync } from "./config/cartSync.ts";
